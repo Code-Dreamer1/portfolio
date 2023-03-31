a499ee5ca56a863ee5b1709e116fb26db6fb4cba
Code-Dreamer1 <faheemwazir333@gmail.com>
2023-03-31 11:22:39 +0500

    initialcommit

---
+++ index.js
@@ -0,0 +1,9 @@
+const nameEl = document.getElementById("name-el");
+
+nameEl.addEventListener("click", function() {
+    nameEl.textContent = "Muhammad Yaseen Wazir";
+})
+
+nameEl.addEventListener("dblclick", function() {
+    nameEl.textContent = "Faheem's Brother";
+})
